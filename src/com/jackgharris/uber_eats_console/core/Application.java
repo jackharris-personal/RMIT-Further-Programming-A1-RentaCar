90399565a89f66d4186a506c3086dd8d80656a4c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-08-22 21:10:49 +1000

    - Added switch statement to build the database instance based on the driver, either sqlite or SQL.

--- src/com/jackgharris/uber_eats_console/core/Application.java
+++ src/com/jackgharris/uber_eats_console/core/Application.java
@@ -2,8 +2,9 @@ package com.jackgharris.uber_eats_console.core;
 
 import com.jackgharris.uber_eats_console.services.FileService;
 
-import javax.xml.crypto.Data;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Application {
 
@@ -18,16 +19,43 @@ public class Application {
         this.directory = System.getProperty("user.dir");
         this.config = FileService.loadConfig(this.directory+"/config.txt");
 
-        Database.instance();
+        List<String> details = new ArrayList<>();
+
+        switch (this.config.get("DB_DRIVER")) {
+            case "mysql" -> {
+
+                details.add(this.config.get("SQL_HOST"));
+                details.add(this.config.get("SQL_PORT"));
+                details.add(this.config.get("SQL_NAME"));
+                details.add(this.config.get("SQL_USER"));
+                details.add(this.config.get("SQL_PASS"));
+
+                Database.boot("mysql", details);
+
+            } case "sqlite" -> {
+
+                details.add(this.config.get("SQLITE_FILE"));
+                Database.boot("sqlite", details);
+
+            }
+        }
+
+        Database.instance().test();
+
+
 
     }
 
-    public static Application instance() {
 
+    public static Application instance() {
         return instance;
     }
 
     public static void boot(){
         instance = new Application();
+    }
+
+    public String getConfig(String key){
+        return this.config.get(key);
     }
 }

cbe266240ef0e410d20268a01a79b3ebd3c5744f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-08-23 21:28:37 +1000

    - Updated main application to reflect the changes seen in the Database class where by it accepts the full config instead of a List<>.

--- src/com/jackgharris/uber_eats_console/core/Application.java
+++ src/com/jackgharris/uber_eats_console/core/Application.java
@@ -1,11 +1,8 @@
 package com.jackgharris.uber_eats_console.core;
 
-import com.jackgharris.uber_eats_console.models.Restaurant;
 import com.jackgharris.uber_eats_console.services.FileService;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class Application {
 
@@ -17,31 +14,8 @@ public class Application {
         this.directory = System.getProperty("user.dir");
         this.config = FileService.loadConfig(this.directory+"/config.txt");
 
-        List<String> details = new ArrayList<>();
+        Database.boot(this.config.get("DB_DRIVER"), this.config);
 
-        switch (this.config.get("DB_DRIVER")) {
-            case "mysql" -> {
-
-                details.add(this.config.get("SQL_HOST"));
-                details.add(this.config.get("SQL_PORT"));
-                details.add(this.config.get("SQL_NAME"));
-                details.add(this.config.get("SQL_USER"));
-                details.add(this.config.get("SQL_PASS"));
-
-            } case "sqlite" -> {
-                details.add(this.config.get("SQLITE_FILE"));
-            }
-        }
-
-        Database.boot(this.config.get("DB_DRIVER"), details);
-
-        Restaurant[] restaurants = Restaurant.all();
-
-        int i = 0;
-        while(i < restaurants.length){
-            System.out.println(restaurants[i].getName());
-            i++;
-        }
     }
 
 }

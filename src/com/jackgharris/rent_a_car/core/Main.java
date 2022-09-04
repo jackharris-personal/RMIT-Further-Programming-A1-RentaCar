743ddc72a381c29ed2888d26cd2f48da5bae0119
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-04 17:47:38 +1000

    - Renamed Launcher.java to Main.java to match assignment specifications. - Added commenting for the Main.java class.

---
+++ src/com/jackgharris/rent_a_car/core/Main.java
@@ -0,0 +1,17 @@
+//**** PACKAGE****\\
+//This specifics what package this specific class this Main.java class
+//is a part of.
+package com.jackgharris.rent_a_car.core;
+
+public class Main {
+
+    //**** JAVA PROGRAM ENTRY POINT ****\\
+    //Our public static void main is the primary entry point for all
+    //java applications, here we can see the only job of the main class
+    //is to create an instance of the Application.
+    public static void main (String[] args){
+
+        new Application();
+
+    }
+}

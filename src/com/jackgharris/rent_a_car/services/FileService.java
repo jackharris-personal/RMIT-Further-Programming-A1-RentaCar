399fa7b0102f536563962bc0ac171e3505c9f2f6
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-04 14:38:33 +1000

    - rebuilt the FileService class to return a ArrayList of HashMap<String, String> containing all the model data for the cars.

--- src/com/jackgharris/rent_a_car/services/FileService.java
+++ src/com/jackgharris/rent_a_car/services/FileService.java
@@ -2,44 +2,72 @@ package com.jackgharris.rent_a_car.services;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class FileService {
 
-    public static HashMap<String, String> loadConfig(String fileLocation){
+    public static ArrayList<HashMap<String, String>> loadCarModels(String fileLocation){
 
-        HashMap<String, String> output = new HashMap<String, String>();
+        String[] keys = new String[11];
+        ArrayList<HashMap<String, String>> output = new ArrayList<>();
+
+        int currentLine = 0;
 
         try{
-
             File file = new File(fileLocation);
             Scanner scanner = new Scanner(file);
 
             while(scanner.hasNextLine()){
+                //set our line to the scanner next line.
+                String[] line = scanner.nextLine().split(",");
 
-                String line = scanner.nextLine();
+                //check if this csv file has a header line, if so then we load the keys into an array for use
+                if(currentLine < 1){
 
-                if(!line.contains("//")){
-
-                    String[] data = line.split("=");
-
-                    if(data.length == 1) {
-                        output.put(data[0], "");
-                    }else{
-                        output.put(data[0], data[1]);
+                    //set our int i counter variable
+                    int i = 0;
+                    //whilst "i" is less than our length of keys we push the line value at i to the key value at i
+                    while(i < keys.length){
+                        keys[i] = line[i];
+                        //lastly we increment i to loop over all the line/key input values;
+                       i++;
                     }
+
+                //else then we are processing the model input not the key input
+                }else{
+
+                    //create our model hashmap of data
+                    HashMap<String, String> model = new HashMap<>();
+
+                    //put all our data values into the hashmap with the respective key
+                    model.put(keys[0],line[0]);
+                    model.put(keys[1],line[1]);
+                    model.put(keys[2],line[2]);
+                    model.put(keys[3],line[3]);
+                    model.put(keys[4],line[4]);
+                    model.put(keys[5],line[5]);
+                    model.put(keys[6],line[6]);
+                    model.put(keys[7],line[7]);
+                    model.put(keys[8],line[8]);
+                    model.put(keys[9],line[9]);
+                    model.put(keys[10],line[10]);
+
+                    output.add(model);
+
                 }
 
+                currentLine++;
+
+
             }
-
-            scanner.close();
-
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
         return output;
+
     }
 
 }

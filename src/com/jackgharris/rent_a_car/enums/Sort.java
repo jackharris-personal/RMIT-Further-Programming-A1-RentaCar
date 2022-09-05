c0e00d3a4a15d10884d5474b7d8fbe757167c74f
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-05 14:21:38 +1000

    - Created Sort enum that contains two constants, "DESCENDING" & "ASCENDING", these are used as the constant inputs for our CarCollection orderBy() method as the order type.

---
+++ src/com/jackgharris/rent_a_car/enums/Sort.java
@@ -0,0 +1,12 @@
+//**** PACKAGE ****\\
+//Set the specific package for this class file
+package com.jackgharris.rent_a_car.enums;
+
+//**** CREATE ENUM ****\\
+//Create our public emun, in this case we call it sort and have two options
+//"Ascending" & "Descending" this makes it easy for the CarCollection to be
+//locked to one of the two input options.
+public enum Sort {
+    DESCENDING,
+    ASCENDING
+}

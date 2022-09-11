e9ec45308c914e0af3ff8410fa0ffd3a36ed712c
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-11 16:34:23 +1000

    - Added handleRequest(Request request) method to the Application class, this accepts a request from a view and then is parsed to the current controller. - Added updateView(Response response) method, this updates the view of the current controller and accepts a Response object. - Added Exit() method, this is called when someone exits the program and closes it gracefully. - Added setActiveController(String controller) method that sets the active controller of the application to that of the key provided. - Added controllers hashmap and added all new controller stances to it in the constructor. - called updateView method from constructor, this is required once. - set default controller to "Menu", this is done via the new activeController private variable.

--- src/com/jackgharris/cosc2288/mycar/core/Application.java
+++ src/com/jackgharris/cosc2288/mycar/core/Application.java
@@ -1,26 +1,51 @@
 package com.jackgharris.cosc2288.mycar.core;
 
-import com.jackgharris.cosc2288.mycar.enums.Sort;
+import com.jackgharris.cosc2288.mycar.controllers.MenuController;
+import com.jackgharris.cosc2288.mycar.controllers.ReservationController;
+import com.jackgharris.cosc2288.mycar.controllers.SearchController;
 import com.jackgharris.cosc2288.mycar.models.Car;
 import com.jackgharris.cosc2288.mycar.services.FileService;
 
+import java.util.HashMap;
+
 
 public class Application {
+
+    private final HashMap<String, Controller> controllers;
+    private String activeController;
 
     public Application(){
 
         String directory = System.getProperty("user.dir");
         Car.bindModelData(FileService.loadCarModels(directory+"/fleet.csv"));
 
+        this.controllers = new HashMap<>();
 
+        this.controllers.put("Menu", new MenuController(this));
+        this.controllers.put("Search", new SearchController(this));
+        this.controllers.put("Reservation", new ReservationController(this));
 
+        this.activeController = "Menu";
 
-        Car[] cars = Car.orderBy("Year of Manufacture", Sort.DESCENDING).get();
+        this.updateView(new Response());
+    }
 
-        for (Car car: cars) {
-            System.out.println("CarAttributes: "+car.getBrand()+" "+car.getModel()+" Year of Manufacture: "+car.getYearOfManufacture());
-        }
+    public void setActiveController(String controller){
+        this.activeController = controller;
+    }
 
+    public void handleRequest(Request request){
+        this.controllers.get(this.activeController).handleRequest(request);
+    }
+
+    public void updateView(Response response){
+        this.controllers.get(this.activeController).updateView(response);
+    }
+
+    public void exit(){
+        System.out.println("Thankyou for using MyCar Console");
+        System.out.println("MyCar is now saving & closing");
+        System.exit(0);
     }
 
 }

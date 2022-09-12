7127f2eb9a5c7b06c604e9a7f8eaa30095bfef5a
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-12 17:40:28 +1000

    - Added commenting for Ascii.java class.

--- src/com/jackgharris/cosc2288/mycar/core/Ascii.java
+++ src/com/jackgharris/cosc2288/mycar/core/Ascii.java
@@ -1,7 +1,17 @@
+//**** PACKAGE ****\\
 package com.jackgharris.cosc2288.mycar.core;
 
+//**** CLASS ****\\
 public class Ascii {
 
-    public static String red = "\u001B[31m";
-    public static String black = "\u001B[0m";
+    //This class does not have a constructor and only has static methods
+    //that relate to coloring or styling ASCII text
+
+    //**** STATIC FINAL RED VARIABLE***
+    //contains the red font styling.
+    public static final String red = "\u001B[31m";
+
+    //**** STATIC FINAL BLACK VARIABLE***
+    //contains the standard black font.
+    public static final String black = "\u001B[0m";
 }

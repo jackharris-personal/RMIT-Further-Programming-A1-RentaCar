2abcf2aad13ece4a3809f5ddb4bae6e08b5051bd
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-15 22:34:19 +1000

    - Created response class, this is returned from the controller to a view and then use by the view to display any data required, its set of methods mirror that of the request with two additions being the frequent use of "containsError" & "getError" for determine if a response has an error that should be shown, and "containsNotification" & "getNotification" to determine if a notification should be shown on the view.

---
+++ src/com/jackgharris/cosc2288/mycar/core/Response.java
@@ -0,0 +1,87 @@
+package com.jackgharris.cosc2288.mycar.core;
+
+import java.util.HashMap;
+
+public class Response {
+
+    private String error;
+    private String notification;
+    private final HashMap<String, Object> data;
+
+    private String redirect;
+
+    public Response(){
+        this.error = "";
+        this.notification = "";
+        this.data = new HashMap<String, Object>();
+    }
+
+    public Response(Request request){
+        this.error = "";
+        this.notification = "";
+        this.data = request.getData();
+    }
+
+    public void add(String key, Object object){
+        this.data.put(key, object);
+    }
+
+    public Object get(String key){
+        return this.data.get(key);
+    }
+
+    public boolean isset(String key){
+        return this.data.containsKey(key);
+    }
+
+    public void setError(String error){
+        this.error = error;
+    }
+
+    public void setNotification(String notification, String color){
+        this.notification = color+notification+Ascii.black;
+    }
+
+    public String getNotification(){
+        return this.notification+"\n";
+    }
+
+    public boolean containsNotification(){
+        return !this.notification.equals("");
+    }
+
+    public boolean containsError(){
+        return !this.error.equals("");
+    }
+
+    public String getError(){
+        return Ascii.red+this.error+Ascii.black;
+    }
+
+    public void setViewRedirect(String redirect){
+        this.redirect = redirect;
+    }
+
+
+    public String getViewRedirect(){
+        return this.redirect;
+    }
+
+
+    public boolean shouldRedirect(){
+        boolean outcome = false;
+
+        if(this.redirect != null){
+            outcome = true;
+        }
+
+        return outcome;
+    }
+
+    public Request cloneDataToRequest(Request request){
+        this.data.forEach(request::add);
+
+        return request;
+    }
+
+}

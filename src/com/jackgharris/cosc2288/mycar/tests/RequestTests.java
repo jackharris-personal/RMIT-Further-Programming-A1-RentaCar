f31f623570a767911ad71dd01fcc94b91d56e281
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-18 12:46:11 +1000

    - Created RequestTests class and written tests for all returnable methods that are part of the request class.

---
+++ src/com/jackgharris/cosc2288/mycar/tests/RequestTests.java
@@ -0,0 +1,95 @@
+package com.jackgharris.cosc2288.mycar.tests;
+
+import com.jackgharris.cosc2288.mycar.core.Request;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RequestTests {
+
+
+    @Test
+    public void requestGetUserInput(){
+
+        Request request = new Request();
+
+        request.setUserInput("Hello World");
+
+        assertTrue(request.getUserInput().matches("Hello World"));
+    }
+
+    @Test
+    public void requestContainsUserInput(){
+
+        Request request = new Request();
+
+        request.setUserInput("Hello World");
+
+        assertTrue(request.containsUserInput());
+    }
+
+    @Test
+    public void requestResetUserInput(){
+        Request request = new Request();
+
+        request.setUserInput("Hello World");
+        request.resetUserInput();
+
+        assertEquals("", request.getUserInput());
+    }
+
+    @Test
+    public void requestGet(){
+        Request request = new Request();
+
+        request.add("test","unit test");
+
+        assertEquals("unit test", request.get("test"));
+    }
+
+    @Test
+    public void requestContainsData(){
+        Request request = new Request();
+
+        request.add("test","unit test");
+
+        assertTrue(request.containsData("test"));
+
+    }
+
+    @Test
+    public void requestSetError(){
+        Request request = new Request();
+
+        request.setError("unit test error");
+
+        assertEquals("unit test error", request.getError());
+    }
+
+    @Test
+    public void requestIsInteger(){
+        Request request = new Request();
+        request.setUserInput("2");
+
+        assertTrue(request.isInteger());
+    }
+
+    @Test
+    public void requestGetUserInputAsInteger(){
+
+        Request request = new Request();
+        request.setUserInput("5");
+        assertEquals(5,request.getUserInputAsInteger());
+    }
+
+    @Test
+    public void requestIsDate(){
+        Request request = new Request();
+
+        request.setUserInput("18/09/2022");
+        assertTrue(request.isDate());
+    }
+
+}

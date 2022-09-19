b6af3322a2cfbcd9ed045ca7b760226442964e45
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-19 16:10:14 +1000

    - Updated the showVehicleDetails view to show the calculations and correct discount totals. - Created the collectedUserDetails view and added all view logic to correctly get and process the user details. - Created the configuration view and added all output logic to show the user the confirmation after completing a booking.

--- src/com/jackgharris/cosc2288/mycar/views/ReservationView.java
+++ src/com/jackgharris/cosc2288/mycar/views/ReservationView.java
@@ -36,23 +36,32 @@ public class ReservationView extends View {
         if(response.containsNotification()){
             System.out.println(response.getNotification());
         }
+
+        System.out.println("\n");
+
         Car car = (Car)response.get("car");
         Reservation reservation = (Reservation)response.get("reservation");
 
         System.out.println("Vehicle:              "+car.getID());
-        System.out.println("Brand:                "+ car.getBrand());
-        System.out.println("Model:                "+ car.getModel());
+        System.out.println("Brand:                "+car.getBrand());
+        System.out.println("Model:                "+car.getModel());
         System.out.println("Type of vehicle:      "+car.getType());
         System.out.println("year of manufacture:  "+car.getYearOfManufacture());
         System.out.println("No. Of Seats:         "+car.getSeatCount());
         System.out.println("Color:                "+car.getColor());
-        System.out.println("Rental:               $"+reservation.getRentalCost());
-        System.out.println("Discount price:       $"+reservation.getDiscountPrice());
-        System.out.println("Insurance:            $"+reservation.getInsuranceCost());
+        System.out.println("Rental:               $"+reservation.getRentalCost()+"     "+reservation.getStandardCalculation());
+
+        if(reservation.hasDiscount()){
+            System.out.println("Discount price:       $"+reservation.getDiscountPrice()+"     "+reservation.getDiscountCalculation());
+        }else{
+            System.out.println("Discount price:       $"+reservation.getDiscountPrice()+"      (Discount is not applicable)");
+        }
+
+        System.out.println("Insurance:            $"+reservation.getInsuranceCost()+"     "+reservation.getInsuranceCalculation());
         System.out.println("Service Fee:          $"+reservation.getServiceFee());
         System.out.println("Total:                $"+reservation.getTotalCost()+"\n");
 
-        System.out.println("Would you like to reserve the vehicle (Y/N)?");
+        System.out.println("Would you like to reserve the vehicle (y/n)?");
 
         if(response.containsError()){
             System.out.println(response.getError());
@@ -63,12 +72,85 @@ public class ReservationView extends View {
 
     public Request collectUserDetails(Response response){
 
-        return this.getUserInput(new Request());
+        Request request = new Request(response);
+
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("> Provide personal information");
+        System.out.println("------------------------------------------------------------------");
+        if(response.containsNotification()){
+            System.out.println(response.getNotification());
+        }
+        System.out.println("\n");
+
+        int  detailsProvided = 0;
+
+        if(response.containsKey("given name")){
+            System.out.println("Given Name: "+response.get("given name"));
+            detailsProvided++;
+        }
+        if(response.containsKey("surname")){
+            System.out.println("Surname: "+response.get("surname"));
+            detailsProvided++;
+        }
+        if(response.containsKey("email")){
+            System.out.println("Email: "+response.get("email"));
+            detailsProvided++;
+        }
+
+        if(response.containsKey("passengers")){
+            System.out.println("Passengers: "+response.get("passengers")+"\n");
+            detailsProvided++;
+        }
+
+        if(detailsProvided < 4){
+            System.out.println(response.get("heading"));
+        }else{
+            System.out.println("Please enter 'y' or 'n' to confirm and pay or cancel and return to the main menu");
+        }
+        System.out.println("\n");
+
+
+        if(response.containsError()){
+            System.out.println(response.getError());
+        }
+
+        return this.getUserInput(request);
     }
 
-    public Request confirmAndPay(Response response){
+    public Request confirmation(Response response){
 
-        return this.getUserInput(new Request());
+        Request request = new Request(response);
+        Reservation reservation = (Reservation) response.get("reservation");
+
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("> Congratulations! Your vehicle is booked.");
+        System.out.println("------------------------------------------------------------------");
+
+        if(response.containsNotification()){
+            System.out.println(response.getNotification());
+        }
+
+        System.out.println("");
+
+        System.out.println("Name:                 "+reservation.getUserGivenName()+" "+reservation.getUserSurname());
+        System.out.println("Email:                "+reservation.getUserEmail());
+        System.out.println("Your Vehicle:         "+reservation.getCar().getBrand()+" "+reservation.getCar().getModel()+" "+reservation.getCar().getType()+" with "+reservation.getCar().getSeatCount()+" seats");
+        System.out.println("Number of passengers: "+reservation.getPassengerCount());
+        System.out.println("Pick-up date:         "+reservation.getPickUpDate());
+        System.out.println("Drop-off date:        "+reservation.getDropOffDate());
+        System.out.println("Total payment:        $"+reservation.getTotalCost());
+
+        System.out.println("");
+
+        System.out.println("1) Return to main menu");
+        System.out.println("2) Exit program");
+
+
+        if(response.containsError()){
+            System.out.println(response.getError());
+        }
+
+        return this.getUserInput(request);
     }
 
 }

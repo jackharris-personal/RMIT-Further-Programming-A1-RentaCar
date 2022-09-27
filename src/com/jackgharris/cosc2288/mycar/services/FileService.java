78d06cf1bf0d5d3e5b581334f76ca44761f1cc13
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-09-27 21:06:07 +1000

    - Finished commenting on the FileService class

--- src/com/jackgharris/cosc2288/mycar/services/FileService.java
+++ src/com/jackgharris/cosc2288/mycar/services/FileService.java
@@ -1,24 +1,37 @@
+//**** SET PACKAGE ****\\
 package com.jackgharris.cosc2288.mycar.services;
 
+//**** PACKAGE IMPORTS ****\\
+//Packages and classes required by this class file
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
+//**** START CLASS ****\\
 public class FileService {
 
+    //**** STATIC LOAD CAR MODEL METHOD ****\\
+    //This method accepts a file location and then returns an array list of hash maps
+    //containing the key value pairs for each model.
     public static ArrayList<HashMap<String, String>> loadCarModels(String fileLocation){
 
+        //create our array of keys, this can be a set size as we only have 11 keys total
         String[] keys = new String[11];
+        //create our output array list
         ArrayList<HashMap<String, String>> output = new ArrayList<>();
 
+        //track what our current line is
         int currentLine = 0;
 
         try{
+            //try to load the file from the file location
             File file = new File(fileLocation);
+            //create our scanner and parse it the file
             Scanner scanner = new Scanner(file);
 
+            //while the scanner has a next line to read then read it
             while(scanner.hasNextLine()){
                 //set our line to the scanner next line.
                 String[] line = scanner.nextLine().split(",");
@@ -54,20 +67,21 @@ public class FileService {
                     model.put(keys[9],line[9]);
                     model.put(keys[10],line[10]);
 
+                    //put this model hashmap into the output array
                     output.add(model);
-
                 }
 
+                //increase our current line
                 currentLine++;
-
-
             }
         } catch (FileNotFoundException e) {
+
+            //else then if we do not find the file then set the output to a new array list
             output = new ArrayList<>();
         }
 
+        //Finally then return the output to the caller
         return output;
-
     }
 
 }

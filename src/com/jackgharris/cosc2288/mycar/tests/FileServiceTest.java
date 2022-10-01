7b87edc1e59f292fc7ee91abf29f0b072d589552
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-01 20:24:13 +1000

    - Created FileServiceTest class with tests for the file service -> loadModelData() method.

---
+++ src/com/jackgharris/cosc2288/mycar/tests/FileServiceTest.java
@@ -0,0 +1,22 @@
+package com.jackgharris.cosc2288.mycar.tests;
+
+import com.jackgharris.cosc2288.mycar.models.Car;
+import com.jackgharris.cosc2288.mycar.services.FileService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FileServiceTest {
+
+    @Test
+    public void loadCarModelsTestWithInValidPath(){
+        Car.bindModelData(FileService.loadCarModels(System.getProperty("user.dir")+"/fleetASDASDSAD.csv"));
+        assertEquals(Car.get().length, 0);
+    }
+
+    @Test
+    public void loadCarModelsTestWithValidPath(){
+        Car.bindModelData(FileService.loadCarModels(System.getProperty("user.dir")+"/fleet.csv"));
+        assertEquals(Car.get().length, 9);
+    }
+}

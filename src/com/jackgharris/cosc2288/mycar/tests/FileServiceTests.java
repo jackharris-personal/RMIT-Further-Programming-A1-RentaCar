48f1ec2b5282d2de7b37eba4ae433b685f41aded
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-10-01 20:25:50 +1000

    - Renamed to FileServiceTests to reflect the fact it contains multiple tests not one!

--- src/com/jackgharris/cosc2288/mycar/tests/FileServiceTests.java
+++ src/com/jackgharris/cosc2288/mycar/tests/FileServiceTests.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class FileServiceTest {
+public class FileServiceTests {
 
     @Test
     public void loadCarModelsTestWithInValidPath(){
